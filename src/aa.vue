dd5883b2aa0e5b8cbdd779ca9e6a841ea24f183a
TorinhoGod <65672170+TorinhoGod@users.noreply.github.com>
2022-10-10 21:52:19 -0300

    Add files via upload

---
+++ src/aa.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>original message: "{{ message }}"</p>
+    <p>reversed message: "{{ reversedMessage }}"</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "Hello"
+    };
+  },
+  computed: {
+    reversedMessage() {
+      return this.message
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
+</script>
