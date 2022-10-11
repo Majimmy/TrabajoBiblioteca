f0d1423d1e9879b982acb5027ca9ed82daef3159
TorinhoGod <65672170+TorinhoGod@users.noreply.github.com>
2022-10-10 21:54:41 -0300

    Add files via upload

---
+++ Frontend/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <h1>Biblioteca mini</h1>
+  <input type="text" v-model="input" placeholder="Busca libros..." />
+ <div class="item libro" v-for="libro in filteredList()" :key="libro">
+   <p>{{ libro }}</p>
+ </div>
+ <div class="item error" v-if="input&&!filteredList().length">
+    <p>No se encontraron resultados</p>
+ </div>  
+</template>
+
+
+<script setup>
+import { ref } from "vue";
+let input = ref("");
+const libros = ["Libro 1", "Libro 2", "Libro 3"];
+function filteredList() {
+  return libros.filter((libro) =>
+    libro.toLowerCase().includes(input.value.toLowerCase())
+  );
+}
+</script>
+
+<style>
+#app {
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  font-style: italic;
+  text-align: center;
+  font-size: xx-large;
+  color: #d5dde2;
+  margin-top: 10px;
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+body {
+  padding: 20px;
+  min-height: 100vh;
+  background-color: rgb(41, 41, 43);
+}
+
+input {
+  display: block;
+  width: 350px;
+  margin: 20px auto;
+  padding: 10px 45px;
+  background: white url("assets/search.svg") no-repeat 15px center;
+  background-size: 15px 15px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+    rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+}
+
+.item {
+  width: 350px;
+  margin: 0 auto 10px auto;
+  padding: 10px 20px;
+  color: rgb(242, 232, 238);
+  border-radius: 5px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
+    rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+}
+
+.libro {
+  background-color: rgb(83, 146, 202);
+  cursor: pointer;
+}
+
+.error {
+  background-color: tomato;
+}
+</style>
