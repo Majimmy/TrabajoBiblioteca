154e837e580534fa40259738c4193fcbca89b9f3
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-10-17 16:56:12 -0300

    Add files via upload
    update union version 1

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <h1>Biblioteca mini</h1>
   <input type="text" v-model="input" placeholder="Busca libros..." />
+  <button @click="update"> Actualizar Lista </button>
  <div class="item libro" v-for="libro in filteredList()" :key="libro">
    <p>{{ libro }}</p>
  </div>
@@ -11,11 +12,22 @@
 
 
 <script setup>
-import { ref } from "vue";
+import { ref } from 'vue';
+import axios from 'axios'
+
 let input = ref("");
-const libros = ["Libro 1", "Libro 2", "Libro 3"];
+let codLibro = ref(0)
+let libros = ref(["Libro 0","Libro 2","Libro 3"]);
+let update = function(){
+  axios
+  .get('http://localhost:3000/libros')
+  .then(response => {
+    codLibro.value = response.data.libros
+    libros.value[0] = "Libro "+codLibro.value /*ejemplo de actualizacion de la lista*/
+  })
+}
 function filteredList() {
-  return libros.filter((libro) =>
+  return libros.value.filter((libro) =>
     libro.toLowerCase().includes(input.value.toLowerCase())
   );
 }
@@ -35,13 +47,11 @@ function filteredList() {
   margin: 0;
   box-sizing: border-box;
 }
-
 body {
   padding: 20px;
   min-height: 100vh;
   background-color: rgb(41, 41, 43);
 }
-
 input {
   display: block;
   width: 350px;
@@ -55,7 +65,6 @@ input {
   box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
     rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
 }
-
 .item {
   width: 350px;
   margin: 0 auto 10px auto;
@@ -65,12 +74,10 @@ input {
   box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
     rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
 }
-
 .libro {
   background-color: rgb(83, 146, 202);
   cursor: pointer;
 }
-
 .error {
   background-color: tomato;
 }
