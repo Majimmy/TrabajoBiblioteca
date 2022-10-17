dcca371d464eb8c310cef455d736b376011c9e6e
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-10-17 16:52:39 -0300

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,11 @@
-/* poner en backend directamente */
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-/*var cors = require('cors') activar cuando se conecte con frontend*/ 
+var cors = require('cors') /*Permite uso de cors*/ 
 var librosRouter = require('./routes/libros');
 var app = express();
-/**app.use(cors())           activar cuando se conecte con frontend*/
+app.use(cors())   /*Utiliza cors*/
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
