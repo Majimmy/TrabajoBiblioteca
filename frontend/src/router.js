5338614cb2368b96d0fb9071b0df62a229c997ba
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-10-24 14:23:14 -0300

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,25 +1,25 @@
 import { createWebHistory, createRouter } from "vue-router";
-// para hacer referencia a las componentes de la pagina web (frontend).
+// para hacer referencia a las componentes de la pagina web (frontend) y moverse entre paginas.
 const routes =  [
   {
     path: "/",
-    alias: "/libros",
+    alias: "/libros", // puede usar el alias o el path
     name: "ver-libros",
-    component: () => import("./componentes/ListaLibros")
+    component: () => import("./componentes/ListaLibros") // refiere a la pagina principal
   },
   {
     path: "/libros/:id",
     name: "detalles-de-libros",
-    component: () => import("./componentes/Libro")
+    component: () => import("./componentes/Libro") // refiere a la pagina que presenta las herramientas para modificar/borrar libro
   },
   {
     path: "/add",
     name: "agregar-libro",
-    component: () => import("./componentes/AgregaLibro")
+    component: () => import("./componentes/AgregaLibro") // regiere a la pagina que presenta herramientas para agregar libro
   }
 ];
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes, // aqui toma la parte con forma routes
 });
-export default router;
+export default router; // se exporta para ser utilizado
