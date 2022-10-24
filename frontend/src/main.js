e9c912fe9236c2dec2df5d891e7311144375cb8b
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-10-24 14:17:59 -0300

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 // se importa bootstrap. Sirve para poder utilizar elementos para la pagina web (como bulma)
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-// se importa router. Router sirve para crear rutas predefinidas. Hacer esto optimiza cantidad de codigo escrito y establece un orden.
+// se importa router. Router sirve para poder utilizar mas de una pagina en la aplicacion web creando rutas predefinidas. 
+//Esto resulta en optimizar cantidad de codigo escrito y establece un orden en los template que ahora se pueden dividir en .js separados.
 import router from './router'
 
 createApp(App).use(router).mount('#app')
