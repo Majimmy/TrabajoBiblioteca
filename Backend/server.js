ae7d301d48eb234320132260eb3a690631929834
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-10-23 21:21:48 -0300

    Add files via upload

---
+++ Backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// requests para los tipo - aplicacion/json
+app.use(bodyParser.json());
+
+// requests para los tipo - aplicacion/x-www-(forma de url)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ruta simple
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenido a el Back-end. Si ves este mensaje es porque el backend esta activo" });
+});
+
+// integracion de mongoDB 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Conectado a Base de datos");
+  })
+  .catch(err => {
+    console.log("No se puede entablar coneccion", err);
+    process.exit();
+  });
+
+require("./app/routes/libro.routes")(app);
+
+// puerto para escuchar los requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server corre en el puerto: ${PORT}.`);
+});
