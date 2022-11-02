c46b4668004c7b8253f3493e04e17474ec9b6e56
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-11-02 15:48:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:8081" // puerto cual escucha
 };
 // utiliza cors
 app.use(cors(corsOptions));
