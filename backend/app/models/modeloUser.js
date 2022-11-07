19ca22f4be1af13638779352a75bdbf4952bb3fd
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-11-07 20:45:07 -0300

    Add files via upload

---
+++ backend/app/models/modeloUser.js
@@ -0,0 +1,16 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+      {
+        nombre: String,
+        rut: {type: String, unique: true, required: true, dropDups: true} // convierte el atributo en unico y rechaza una duplicacion
+      },
+      { timestamps: true }
+    );
+    schema.method("toJSON", function() {
+      const { __v, _id, ...object } = this.toObject();
+      object.id = _id;
+      return object;
+    });
+    const User = mongoose.model("user", schema);
+    return User;
+  };
