90ab4ff79de9be11020619165c7b711d51c1e42a
GuiInfoU <115105695+GuiInfoU@users.noreply.github.com>
2022-11-08 00:16:54 -0300

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -10,6 +10,9 @@
         <li class="nav-item">
           <router-link to="/add" class="nav-link">Agregar</router-link>
         </li>
+        <li class="nav-item">
+          <router-link to="/add2" class="nav-link">Usuarios</router-link>
+        </li>
       </div>
     </nav>
     <div class="container mt-3">
